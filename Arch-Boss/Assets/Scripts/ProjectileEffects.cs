94b1caeaeb757c992b741a5c80b40419668a620c
AMONGUS <wizardoftx5@gmail.com>
2026-08-30 23:01:21 +0800

    Added ProjectileEffects.cs, custom effects inherit from this class

--- Arch-Boss/Assets/Scripts/ProjectileEffects.cs
+++ Arch-Boss/Assets/Scripts/ProjectileEffects.cs
@@ -3,24 +3,30 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class ProjectileEffects : MonoBehaviour {
     public int Damage = 0;
+
+    /// <summary> Type of dmg reduction the entity will use against this projectile. </summary>
     public DamageType Type = DamageType.Physical;
-    public int Pierce = 0; // number of entities it can pass through before dying. Negative for infinite
+
+    /// <summary> number of entities it can pass through before dying. Negative for infinite </summary>
+    public int Pierce = 0;
     public float TimeToLive = 1f;
-    public string Owner = " "; // projectile will not damage its owner
+
+    /// <summary> Tag of owner, projectile will not interact with owner tag </summary>
+    public string OwnerTag = " ";
 
     private void Update() {
         TimeToLive -= Time.deltaTime;
         if (TimeToLive <= 0) {
-            Destroy(transform.root.gameObject);
+            Destroy(transform.root.gameObject); // CHANGE THIS WHEN WE USE POLLING SYSTEM
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collider) {
         GameObject hitObject = collider.gameObject;
-        if (hitObject.layer == LayerMask.NameToLayer("Entity") && !hitObject.CompareTag(Owner)) {
+        if (hitObject.layer == LayerMask.NameToLayer("Entity") && !hitObject.CompareTag(OwnerTag)) {
             Interact(hitObject);
             if (Pierce-- == 0) {
-                Destroy(transform.root.gameObject);
+                Destroy(transform.root.gameObject); // CHANGE THIS WHEN WE USE POLLING SYSTEM
             }
         }
     }
