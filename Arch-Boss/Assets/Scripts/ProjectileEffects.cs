126e20c2dd7588957f5b0170b4e6acecffd4f81a
AMONGUS <wizardoftx5@gmail.com>
2026-09-01 22:14:38 +0800

    removed (bad) iframe code from projectileEffects

--- Arch-Boss/Assets/Scripts/ProjectileEffects.cs
+++ Arch-Boss/Assets/Scripts/ProjectileEffects.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class ProjectileEffects : MonoBehaviour {
-    private float cooldownTimer = 0f;
     public int Damage = 0;
 
     /// <summary> Type of dmg reduction the entity will use against this projectile. </summary>
@@ -11,16 +10,11 @@ public class ProjectileEffects : MonoBehaviour {
     /// <summary> number of entities it can pass through before dying. Negative for infinite </summary>
     public int Pierce = 0;
     public float TimeToLive = 1f;
-
-    /// <summary> Time before the same projectile can affect the same entity again </summary>
-    public float EffectCooldown = 0.5f;
-
     /// <summary> Tag of owner, projectile will not interact with owner tag </summary>
     public string OwnerTag = "Placeholder";
     public ProjectilePool PoolingSystem;
 
     private void Update() {
-        cooldownTimer -= Time.deltaTime;
         TimeToLive -= Time.deltaTime;
         if (TimeToLive <= 0) {
             PoolingSystem.Release(gameObject);
@@ -29,10 +23,8 @@ public class ProjectileEffects : MonoBehaviour {
 
     private void OnTriggerStay2D(Collider2D collider) {
         GameObject hitObject = collider.gameObject;
-        Debug.Log("hi");
-        if (hitObject.layer == LayerMask.NameToLayer("Entity") && !hitObject.CompareTag(OwnerTag) && cooldownTimer <= 0) {
+        if (hitObject.layer == LayerMask.NameToLayer("Entity") && !hitObject.CompareTag(OwnerTag)) {
             Interact(hitObject);
-            cooldownTimer = EffectCooldown;
             if (Pierce-- == 0) {
                 PoolingSystem.Release(gameObject);
             }
