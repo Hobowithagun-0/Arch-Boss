0a2e56992312806935fa5adc9e8744e5232aa85d
AMONGUS <wizardoftx5@gmail.com>
2026-09-06 11:37:46 +0800

    Added FloatyBoss who can shoot arrows and dash while flying

--- Arch-Boss/Assets/Scripts/Boss/FloatyBoss.cs
+++ Arch-Boss/Assets/Scripts/Boss/FloatyBoss.cs
@@ -1,31 +1,85 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(ProjectilePool))]
 [RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class FloatyBoss : MonoBehaviour {
+    private readonly float[] directions = { -1f, 1f };
     private Rigidbody2D body;
     private InputAction move;
+    private InputAction attack;
     private InputAction special;
+    private ProjectilePool projPool;
+    private float attackDelta;
+    private float dashDelta;
     private float rateX;
     private float rateY;
-    public float MoveSpeed = 1f;
-    public float Accel = 1f;
+    public float MaxSpeed = 1f;
+    public float TimeToMaxSpeed = 1f;
+    public float AttackCooldown = 1f;
+    public float ArrowSpeed = 1f;
+    public float ArrowSpread = 1f;
+    public float DashCooldown = 1f;
+    public float DashSpeed = 1f;
 
     void Start() {
         body = GetComponent<Rigidbody2D>();
+        projPool = GetComponent<ProjectilePool>();
 
         move = InputSystem.actions.FindAction("Move", true);
+        attack = InputSystem.actions.FindAction("Attack", true);
         special = InputSystem.actions.FindAction("Special", true);
+
+        dashDelta = DashCooldown;
+        attackDelta = AttackCooldown;
+    }
+
+    private void Update() {
+        if (special.WasPressedThisFrame() && dashDelta >= DashCooldown) {
+            Dash();
+            dashDelta = 0f;
+        } else {
+            dashDelta += Time.deltaTime;
+        }
+        if (attack.WasPressedThisFrame() && attackDelta >= AttackCooldown) {
+            ShootArrows();
+            attackDelta = 0f;
+        } else {
+            attackDelta += Time.deltaTime;
+        }
     }
 
     private void FixedUpdate() {
         Vector2 targetVelo = Vector2.ClampMagnitude(move.ReadValue<Vector2>(), 1f);
-        //body.linearVelocityY = Mathf.MoveTowards(body.linearVelocity.y, MoveSpeed * move.ReadValue<Vector2>().y, Accel);
-        //body.linearVelocityX = Mathf.MoveTowards(body.linearVelocity.x, MoveSpeed * move.ReadValue<Vector2>().x, Accel);
-        body.linearVelocityY = Mathf.SmoothDamp(body.linearVelocity.y, MoveSpeed * targetVelo.y,
-            ref rateY, Accel);
-        body.linearVelocityX = Mathf.SmoothDamp(body.linearVelocity.x, MoveSpeed * targetVelo.x,
-            ref rateX, Accel);
+        body.linearVelocityY = Mathf.SmoothDamp(body.linearVelocity.y, MaxSpeed * targetVelo.y,
+            ref rateY, TimeToMaxSpeed);
+        body.linearVelocityX = Mathf.SmoothDamp(body.linearVelocity.x, MaxSpeed * targetVelo.x,
+            ref rateX, TimeToMaxSpeed);
+    }
+
+    private void Dash() {
+        Vector2 direction = Vector2.Normalize(Camera.main.ScreenToWorldPoint(Pointer.current.position.ReadValue())
+            - transform.position);
+        body.linearVelocity = direction * DashSpeed;
+    }
+
+    private void ShootArrows() {
+        Vector2 direction = Vector2.Normalize(Camera.main.ScreenToWorldPoint(Pointer.current.position.ReadValue())
+            - transform.position);
+        for (int i = 0; i < 3; i++) { // 3 arrows on each side, 6 total
+            foreach (float side in directions) {
+                if (i == 0 && side == 1f) {
+                    continue; // discard 1 middle arrow so 5 total
+                }
+                
+                GameObject Arrow = projPool.Get(); // assume all values except speed, position and projpool are set
+                float angle = Mathf.Atan2(direction.y, direction.x);
+                Arrow.transform.position = transform.position;
+                Arrow.transform.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg + ArrowSpread * side * i);
+                Arrow.GetComponent<Rigidbody2D>().linearVelocity = Arrow.transform.rotation * Vector3.right * ArrowSpeed;
+                Arrow.GetComponent<ProjectileEffects>().PoolingSystem = projPool;
+            }
+        }
     }
 }
