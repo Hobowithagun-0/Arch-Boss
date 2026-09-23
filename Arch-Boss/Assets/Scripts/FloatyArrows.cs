3e4603903aebe7f374c277335add585ef2e31b4d
AMONGUS <wizardoftx5@gmail.com>
2026-09-23 21:01:24 +0800

    Made FloatyArrows use the new variables from parent class instead of fetching its own boxcollider

--- Arch-Boss/Assets/Scripts/FloatyArrows.cs
+++ Arch-Boss/Assets/Scripts/FloatyArrows.cs
@@ -1,13 +1,9 @@
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
 public class FloatyArrows : ProjectileEffects {
 
-    private BoxCollider2D col;
-
     private void Start() {
-        col = GetComponent<BoxCollider2D>();
-        col.includeLayers |= LayerMask.GetMask("Ground"); // adds ground layer to collision checks
+        boxCollider.includeLayers |= LayerMask.GetMask("Ground"); // adds ground layer to collision checks
     }
 
     private void OnTriggerEnter2D(Collider2D collider) {
